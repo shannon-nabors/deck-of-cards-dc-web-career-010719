d13a52cd04a4d971142d0069350a24d24b75ceb8
shannon-nabors <shannon.nabors@gmail.com>
2019-01-24 09:54:09 -0500

    done

--- solution.rb
+++ solution.rb
@@ -3,31 +3,24 @@ require 'pry'
 class Card
   attr_reader :rank, :suit
 
-  def initialize(rank, suit)
-    @rank, @suit = rank, suit
+  def initialize(suit, rank)
+    @suit, @rank = suit, rank
   end
-end
-
-def create_deck
-  ranks = %w(A 2 3 4 5 6 7 8 9 10 J Q K)
-  suits = %w(Hearts Clubs Diamonds Spades)
-  cards = ranks.map do |r|
-    suits.map { |s| Card.new(r,s) }
-  end
-  cards.flatten
 end
 
 class Deck
   attr_accessor :cards
 
-  def initialize(cards)
-    @cards = cards
+  def initialize
+    ranks = %w(A 2 3 4 5 6 7 8 9 10 J Q K)
+    suits = %w(Hearts Clubs Diamonds Spades)
+    cards = ranks.map do |r|
+      suits.map { |s| Card.new(r,s) }
+    end
+    @cards = cards.flatten
   end
 
   def choose_card
     self.cards.delete_at(rand(1..52))
   end
 end
-
-deck = Deck.new(create_deck)
-deck.choose_card
